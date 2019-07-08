843b834a1d524d61edeb91134f31e981e2030be3
Yash Kulshrestha <yash.kulshrestha@gmail.com>
2019-07-08 15:32:52 -0700

    we now have subcommands for talk-writer

--- src/bin.ts
+++ src/bin.ts
@@ -5,23 +5,39 @@ import { Configuration } from 'webpack';
 
 import buildTalk from '.';
 
-const options = yargs
-  .scriptName('talk-writer')
-  .usage('$0 [args]')
-  .option('config', {
-    alias: 'c',
-    describe: 'the config file to use',
-    demand: true,
-    string: true
-  })
-  .option('development', {
-    alias: 'dev',
-    describe: 'whether to run Webpack in development mode or not',
-    demand: true,
-    boolean: true
-  })
-  .help().argv;
-
-const incomingConfig: Partial<Configuration> = require(options.config);
-
-buildTalk(incomingConfig, options.config, options.development);
+yargs
+  .command(
+    ['build', '$0'],
+    'build the application assets',
+    y =>
+      y
+        .option('config', {
+          alias: 'c',
+          describe: 'the config file to use',
+          demand: true,
+          type: 'string'
+        })
+        .option('development', {
+          alias: 'dev',
+          describe: 'whether to run Webpack in development mode or not',
+          demand: true,
+          type: 'boolean'
+        }),
+    args => {
+      const incomingConfig: Partial<Configuration> = require(args.config);
+      buildTalk(incomingConfig, args.config, args.development);
+    }
+  )
+  .command(
+    'generate',
+    'generate the minimal required boilerplate',
+    y =>
+      y.option('language', {
+        alias: 'l',
+        describe: 'the language that you want to write the project in',
+        demand: true,
+        choices: ['typescript', 'ts', 'javascript', 'js']
+      }),
+    args => console.log(args)
+  )
+  .help();
